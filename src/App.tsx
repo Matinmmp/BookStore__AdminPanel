ec38331f286130888c63e34ca5645ec728f41d6e
Matinmmp1381 <matinmmp1381@gmail.com>
2023-08-08 06:01:42 -0700

    add home and login routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
+import routes from './routes/routes';
+import './App.css';
 
 
 function App() {
-
     return (
-        <div>
-
-        </div>
+        <RouterProvider router={routes} />
     )
 }
 
