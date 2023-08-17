1c131458c5575b1ef5083a39a2a2a7c7184b8bbd
Matinmmp1381 <matinmmp1381@gmail.com>
2023-08-16 22:41:23 -0700

    add some changes

--- src/components/Category/Row.tsx
+++ src/components/Category/Row.tsx
@@ -29,8 +29,8 @@ const Row = ({ category, id }: IProps) => {
 
         // </tr>
 
-        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 text-white">
+            <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap dark:text-white">
                 {id}
             </th>
             <td className="px-6 py-4">
